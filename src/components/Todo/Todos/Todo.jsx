d90b3a973d6dbe06eb5c7f5bd1c261267259a80a
Alexey Masasin <alexeymasasin@gmail.com>
2024-06-07 10:13:32 +0500

    "Important" Functionality Update

--- src/components/Todo/Todos/Todo.jsx
+++ src/components/Todo/Todos/Todo.jsx
@@ -1,13 +1,18 @@
 import { RiTodoLine } from 'react-icons/ri';
 import { MdDeleteOutline, MdDone } from 'react-icons/md';
 import styles from './Todo.module.css';
+import { useState } from 'react';
 
 function Todo({ deleteTodo, todo, toggleTodo }) {
+  const [isImportant, setIsImportant] = useState(false);
   return (
     <div className={`${styles.todo} 
       ${todo.isCompleted ? styles.completed_todo : ''}`}>
       <div className={styles.todo_text}>
-        <RiTodoLine className={styles.todo_icon}/>
+        <RiTodoLine onClick={() => setIsImportant(!isImportant)}
+                    className={`${styles.todo_icon} ${isImportant
+                      ? styles.important
+                      : ''}`}/>
         {todo.text}
       </div>
       <MdDeleteOutline onClick={() => deleteTodo(todo.id)}
