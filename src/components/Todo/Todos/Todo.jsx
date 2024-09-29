60f70ba366752577b00e674c95e440ff07540266
Alexey Masasin <alexeymasasin@gmail.com>
2024-09-29 17:30:23 +0500

    Todo cursor fix

--- src/components/Todo/Todos/Todo.jsx
+++ src/components/Todo/Todos/Todo.jsx
@@ -1,69 +1,71 @@
-import { RiTodoLine } from 'react-icons/ri';
-import { MdDeleteOutline, MdDone } from 'react-icons/md';
-import { useTranslation } from 'react-i18next';
 import { motion, Reorder, useDragControls } from 'framer-motion';
+import { useTranslation } from 'react-i18next';
+import { MdDeleteOutline, MdDone } from 'react-icons/md';
+import { RiTodoLine } from 'react-icons/ri';
 import styles from './Todo.module.css';
 import TodoMoveIcon from './TodoMoveIcon';
 
-function Todo({
-  deleteTodo,
-  todo,
-  toggleTodo,
-  importantTodo,
-  constraintsRef,
-}) {
-  const { t } = useTranslation();
+function Todo({ deleteTodo, todo, toggleTodo, importantTodo, constraintsRef }) {
+	const { t } = useTranslation();
 
-  const dragControls = useDragControls();
+	const dragControls = useDragControls();
 
-  return (
-    <motion.div
-      transition={{ duration: 0.2, delay: 0.2 }}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      key="todo"
-      exit={{ opacity: 0, transition: { delay: 0.2 } }}
-    >
-      <Reorder.Item
-        className={styles.reorder_item}
-        value={todo} key={todo.id}
-        dragListener={false}
-        dragControls={dragControls}
-        dragConstraints={constraintsRef}
-        dragTransition={{
-          bounceStiffness: 300,
-          bounceDamping: 50,
-        }}
-        exit={{
-          height: 0,
-          transition: { delay: 0.3, duration: 0.3 },
-        }}
-      >
-        <motion.div className={`${styles.todo}  
+	return (
+		<motion.div
+			transition={{ duration: 0.2, delay: 0.2 }}
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			key="todo"
+			exit={{ opacity: 0, transition: { delay: 0.2 } }}
+		>
+			<Reorder.Item
+				className={styles.reorder_item}
+				value={todo}
+				key={todo.id}
+				dragListener={false}
+				dragControls={dragControls}
+				dragConstraints={constraintsRef}
+				dragTransition={{
+					bounceStiffness: 300,
+					bounceDamping: 50,
+				}}
+				exit={{
+					height: 0,
+					transition: { delay: 0.3, duration: 0.3 },
+				}}
+			>
+				<motion.div
+					className={`${styles.todo}  
       ${todo.isCompleted ? styles.completed : ''}
-      ${todo.isImportant ? styles.important_todo : ''}`}>
-          <motion.div className={styles.todo_left}>
-            <RiTodoLine title={t('IMPORTANT_TODO')}
-                        onClick={() => importantTodo(todo.id)}
-                        className={`${styles.todo_icon} ${todo.isImportant
-                          ? styles.important
-                          : ''}`}/>
-          </motion.div>
-          <div className={styles.todo_text}>{todo.text}</div>
-          <div className={styles.todo_right}>
-            <MdDone title={t('COMPLETE_TODO')}
-                    onClick={() => toggleTodo(todo.id)}
-                    className={styles.todo_done_icon}/>
-            <MdDeleteOutline title={t('DELETE_TODO')}
-                             onClick={() => deleteTodo(todo.id)}
-                             className={styles.todo_delete_icon}/>
-            <TodoMoveIcon dragControls={dragControls}
-            />
-          </div>
-        </motion.div>
-      </Reorder.Item>
-    </motion.div>
-  );
+      ${todo.isImportant ? styles.important_todo : ''}`}
+				>
+					<motion.div className={styles.todo_left}>
+						<RiTodoLine
+							title={t('IMPORTANT_TODO')}
+							onClick={() => importantTodo(todo.id)}
+							className={`${styles.todo_icon} ${
+								todo.isImportant ? styles.important : ''
+							}`}
+						/>
+					</motion.div>
+					<div className={styles.todo_text}>{todo.text}</div>
+					<div className={styles.todo_right}>
+						<MdDone
+							title={t('COMPLETE_TODO')}
+							onClick={() => toggleTodo(todo.id)}
+							className={styles.todo_done_icon}
+						/>
+						<MdDeleteOutline
+							title={t('DELETE_TODO')}
+							onClick={() => deleteTodo(todo.id)}
+							className={styles.todo_delete_icon}
+						/>
+						<TodoMoveIcon dragControls={dragControls} />
+					</div>
+				</motion.div>
+			</Reorder.Item>
+		</motion.div>
+	);
 }
 
 export default Todo;
--- src/components/Todo/Todos/TodoMoveIcon.jsx
+++ src/components/Todo/Todos/TodoMoveIcon.jsx
@@ -1,19 +1,21 @@
-import { CgMenuGridR } from 'react-icons/cg';
-import { useTranslation } from 'react-i18next';
 import { motion } from 'framer-motion';
+import { useTranslation } from 'react-i18next';
+import { CgMenuGridR } from 'react-icons/cg';
 import styles from './Todo.module.css';
 
 function TodoMoveIcon({ dragControls }) {
-  const { t } = useTranslation();
-  return (
-    <motion.div style={{ touchAction: 'none' }}
-                whileTap={{ cursor: 'grabbing' }}
-                whileDrag={{ cursor: 'grabbing' }} whileHover={{ scale: 1.2 }}
-                onPointerDown={(event) => dragControls.start(event)}>
-      <CgMenuGridR className={styles.todo_move_icon} title={t('MOVE_TODO')}
-      />
-    </motion.div>
-  );
+	const { t } = useTranslation();
+	return (
+		<motion.div
+			style={{ touchAction: 'none' }}
+			whileTap={{ cursor: 'grabbing' }}
+			whileDrag={{ cursor: 'grabbing' }}
+			whileHover={{ scale: 1.2, cursor: 'grab' }}
+			onPointerDown={(event) => dragControls.start(event)}
+		>
+			<CgMenuGridR className={styles.todo_move_icon} title={t('MOVE_TODO')} />
+		</motion.div>
+	);
 }
 
 export default TodoMoveIcon;
